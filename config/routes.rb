7a37a25fcc89c9724956ad27f5b60bbcd667d9ca
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-02-12 12:11:25 -0600

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   
+  resources :users, only: [:index, :show, :create]
+  resources :publishers, only: [:index, :show, :create]
   resources :books
-  resources :users
-  resources :publishers
+  
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
