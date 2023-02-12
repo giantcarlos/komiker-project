58ee92bbc018e1aa4d17fca724bd70657ec56ee5
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-02-12 12:23:40 -0600

    add publishers controller actions

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -1,2 +1,34 @@
 class PublishersController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :not_valid
+    rescue_from ActiveRecord::RecordNotFound, with: :no_route
+    
+        def index
+            render json: Publisher.all, status: :ok
+        end
+        
+        def show
+            publisher = Publisher.find_by(id: params[:id])
+            render json: publisher, status: :found
+        end
+    
+        def create
+            publisher = Publisher.create!( publisher_params )
+            render json: publisher, status: :created
+        end
+    
+        private
+    
+        def publisher_params
+            params.permit(:name)
+        end
+    
+        def not_valid(invalid)
+            render json: { error: invalid.record.errors.full_messages }, status: 422
+        end
+    
+        def no_route
+            render json: {error: "Title not found"}, status: :not_found
+        end
+    
 end
+    
