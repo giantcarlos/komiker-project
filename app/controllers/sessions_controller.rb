cacf0f00a316c3e02c6126e193e93641fc6b95a5
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-02-12 12:32:25 -0600

    add create and destroy actions for sesssons controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+          session[:current_user] = user.id
+          render json: user, status: :ok
+        else
+          render json: { error: ["Invalid username and/or password"] }, status: :unauthorized
+        end
+    end
+
+    def destroy
+        session.delete :current_user
+        head :no_content
+    end
+
 end
+
