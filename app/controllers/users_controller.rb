5922e4943e3807e885ae7c3d679f48b418d9a55c
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-02-28 18:52:25 -0600

    edit users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     def create
         user = User.create!( user_params )
         if user.valid?
+            session[:user_id] = user.id
             render json: user, status: :created
         else    
             render json: { errors: user.error.full_messages }, status: :unprocessable_entity
@@ -12,7 +13,7 @@ class UsersController < ApplicationController
     private
     
     def user_params
-        params.permit(:username, :password)
+        params.permit(:username, :email, :password)
     end
     
 end
