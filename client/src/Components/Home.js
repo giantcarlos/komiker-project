bf476fe1855851d119833a9b64cc2aa4660255bf
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-03-01 20:35:43 -0600

    add login form

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Login from './Login';
 
 function Home({ user, setUser }) {
-  if (!user) return <Login onLogin={setUser} />;
+  if (!user) return <Login setUser={setUser} />;
   
   return (
     <div>Home</div>
--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-function Login({ onLogin }) {
+function Login({ setUser }) {
   const [ username, setUsername ] = useState("");
   const [ password, setPassword ] = useState("");
 
@@ -14,16 +14,31 @@ function Login({ onLogin }) {
       body: JSON.stringify({ username, password }),
     }).then((r) => {
       if (r.ok) {
-        r.json().then((user) => onLogin(user))
+        r.json().then((user) => setUser(user))
       }
     })
   }
 
   return (
     <form onSubmit={handleSubmit}>
-
+      <h2>Log in to your Komiker account</h2>
+      <label htmlFor="username">Username: </label>
+      <input 
+        type="text"
+        id="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <label htmlFor="password">Password: </label>
+      <input 
+        type="text"
+        id="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
     </form>
-  )
+  );
 }
 
 export default Login
