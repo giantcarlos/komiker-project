ea68e890036df71ecbbd7dbcda7e4f8c731e60a9
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-03-07 12:57:01 -0600

    add book and publisher validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,9 @@
 class Book < ApplicationRecord
   belongs_to :user
   belongs_to :publisher
+
+  validates :name, presence: true, uniqueness: true
+  validates :writer, presence: true
+  validates :edition, presence: true
+  validates :image_url, presence: true
 end
--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,4 +1,6 @@
 class Publisher < ApplicationRecord
     has_many :books
     has_many :users, through: :books
+
+    validates :name, presence: true, uniqueness: true
 end
