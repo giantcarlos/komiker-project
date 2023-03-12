71f8cd3006ff0ca8022a6302a9796940054d3b6f
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-03-12 18:14:28 -0500

    capitalize username in serializer

--- app/serializers/publisher_serializer.rb
+++ app/serializers/publisher_serializer.rb
@@ -2,3 +2,5 @@ class PublisherSerializer < ActiveModel::Serializer
   attributes :id, :name, :books
   has_many :books
 end
+
+
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,9 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :email
   has_many :books
   has_many :publishers, through: :books
+
+  def username
+    object.username.capitalize
+  end
+
 end
