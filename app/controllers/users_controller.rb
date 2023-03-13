ab3166ab4d354f9ded3cd0f0d00c4edb851a6767
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-03-13 16:28:40 -0500

    move current_user to application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,7 @@ class UsersController < ApplicationController
     end 
 
     def show
-        user = User.find_by(id: session[:user_id])
-        render json: user
+        render json: current_user
     end
     
     private
