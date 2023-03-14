b827f83d63cc205bb9d14cfc94ffe2d09fda6d2a
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-03-14 11:34:49 -0500

    add user_books custom method in UserSerializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,20 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email
+  attributes :id, :username, :email, :publishers, :user_books
   has_many :books
   has_many :publishers, through: :books
 
   def username
     object.username.capitalize
+  end
+
+  def user_books
+    object.publishers.map do |p| 
+      publisher_books=p.books.select {|b| b.user_id==object.id}
+      byebug
+      updated_publisher=p
+      updated_publisher.books=publisher_books
+      updated_publisher
+    end
   end
 
 end
