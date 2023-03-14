3704769f5f59d7692905be070a4d461034f58d71
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-03-14 15:08:39 -0500

    edit serializers

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,5 +1,3 @@
 class BookSerializer < ActiveModel::Serializer
   attributes :id, :name, :writer, :edition, :image_url, :publisher_id, :publisher
-  # belongs_to :user
-  # belongs_to :publisher
 end
--- app/serializers/publisher_serializer.rb
+++ app/serializers/publisher_serializer.rb
@@ -1,6 +1,4 @@
 class PublisherSerializer < ActiveModel::Serializer
   attributes :id, :name, :books
-  has_many :books
-  # has_many :users, through: :books
 end
 
